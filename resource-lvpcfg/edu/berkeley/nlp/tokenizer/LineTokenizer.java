5f1518c2b3749863949b45d152f6136601b5d064
Marten van Schijndel <vanschm@ling.osu.edu>
2017-04-07 22:10:14 -0400

    cleaned up resource-lvpcfg;
    changed all makefiles to use external jar file;
    removed internal java;

--- resource-lvpcfg/edu/berkeley/nlp/tokenizer/LineTokenizer.java
+++
@@ -1,8 +0,0 @@
-package edu.berkeley.nlp.tokenizer;
-
-import java.io.IOException;
-import java.util.List;
-
-public interface LineTokenizer {
-	public List<String> tokenizeLine(String line) throws IOException;
-}
