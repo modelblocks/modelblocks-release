ad4543b9d08d26d94afb46e384519f13271845e1
Cory Shain <cory.shain@gmail.com>
2017-04-23 08:00:11 -0400

    changed rt pipeline to spill over everything

--- resource-rt/scripts/spilloverMetrics.py
+++ resource-rt/scripts/spilloverMetrics.py
@@ -2,19 +2,25 @@ import sys, argparse, pandas as pd
 argparser = argparse.ArgumentParser('''
 Generates spillover metrics from user-supplied columns in tokmeasures input, skipping over punctuation
 ''')
-argparser.add_argument('cols', nargs='+', help='Names of columns for which to add spillover metrics')
+argparser.add_argument('cols', nargs='*', help='Names of columns for which to add spillover metrics')
 argparser.add_argument('-n', '--number', dest='n', action='store', nargs=1, default=[1], help='Number of spillover shifts to calculate.')
 argparser.add_argument('-p', '--skippunc', dest='p', action='store_true', help='Skip punctuation when shifting for spillover calculation.')
+argparser.add_argument('-a', '--allcols', dest='a', action='store_true', help='Spillover all columns.')
 args, unknown = argparser.parse_known_args()
 args.n = int(args.n[0])
 
 punc = ["-LRB-", "-RRB-", ",", "-", ";", ":", "\'", "\'\'", '\"', "`", "``", ".", "!", "?", "*FOOT*"]
 
 data = pd.read_csv(sys.stdin, sep=' ', skipinitialspace=True)
+if args.a:
+    cols = data.columns.values
+else:
+    cols = args.cols 
+
 if args.p:
     data_no_punc = data[~data['word'].isin(punc)]
 
-for col in args.cols:
+for col in cols:
     for i in range(1,args.n+1):
         s_name = col + 'S' + str(i)
         if i == 1:
