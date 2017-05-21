b2adc7083766d39c0a3ffd76fbb166dbb3346313
schuler.77 <schuler.77@deasy.ling.ohio-state.edu>
2017-05-21 00:13:17 -0400

    bug fix to removal of carriers in tokdecs2linetrees

--- resource-incrsem/scripts/tokdecs2linetrees.py
+++ resource-incrsem/scripts/tokdecs2linetrees.py
@@ -13,8 +13,8 @@ for s in sys.stdin:
     s = re.sub( '/[^ ;/]*\^;[^ ;/]*', '', s )   ## remove bottom carriers. (OBSOLETE)
     s = re.sub( ';$', '', s )                   ## remove trailing ;
     s = re.sub( '/ ', ' ', s )                  ## remove trailing /
-    s = re.sub( '/([^/; ]*)/', '/', s )          ## remove A carriers
-    s = re.sub( ';([^/; ]*);', ';', s )          ## remove B carriers
+    s = re.sub( '/([^; ]*)/', '/', s )          ## remove A carriers
+    s = re.sub( ';([^/ ]*);', ';', s )          ## remove B carriers
     m = re.search('^([^ ]*) (?!pos)(?:\[[^\]]*\]:)?([^ ]*) ([^ ]*) ([^ ]*) (?:.*;)?([^ ;\n]*)[ \n]',s)
     if m is not None:
         w,p,f,j,q = m.groups()
