c6f02a0886161905e65e97fc887ba96091bfbdda
Cory Shain <cory.shain@gmail.com>
2017-06-14 11:59:24 -0400

    generalized fmri correlation analysis to all fmri datasets

--- resource-fmri/scripts/corrAnalysis.r
+++ resource-fmri/scripts/corrAnalysis.r
@@ -5,14 +5,14 @@ options(width=500)
 df = read.csv(file('stdin'), header=TRUE, comment.char = '', quote = '', sep = ' ')
 dfw = reshape(df, timevar='subject', idvar='sampleid', direction='wide')
 subjs = levels(df$subject)
-rois = c('LPostTemp','LAntTemp','LAngG','LIFG','LMFG','LIFGorb')
+rois = colnames(df)[grep('^bold', colnames(df))]
 
 cat('====================================================\n')
 cat('Between-subject BOLD response correlations by region\n')
 cat('====================================================\n\n')
 
 for (r in rois) {
-colnames = paste0('bold', r, '.', subjs)
+colnames = paste0(r, '.', subjs)
 print(cor(dfw[colnames]))
 cat('\n')
 }
@@ -21,9 +21,9 @@ cat('===============================================================\n')
 cat('Between-subject BOLD response correlations averaged over region\n')
 cat('===============================================================\n\n')
 
-responseVars = paste0('bold', rois, '.')
+responseVars = paste0(rois, '.')
 for (s in subjs) {
-dfw[paste0('bold', 'Mean.', s)] = rowMeans(dfw[paste0('bold', rois, '.', s)])
+dfw[paste0('boldMean.', s)] = rowMeans(dfw[paste0(rois, '.', s)])
 }
 meanResponseVars = paste0('boldMean.', subjs)
 print(cor(dfw[meanResponseVars]))
@@ -38,13 +38,12 @@ for (s in subjs) {
     grandx[[s]] = 0
 }
 grandmeanrho = 0
-grandSEM = 0
 nregions = length(rois)
 
 for (r in rois) {
 meanrho = 0
 samplesize = 0
-colnames = paste0('bold', r, '.', subjs)
+colnames = paste0(r, '.', subjs)
 for (i in 1:length(colnames)) {
 x = as.numeric(cor(dfw[colnames[i]], rowMeans(dfw[colnames[-i]])))
 grandx[[subjs[i]]] = grandx[[subjs[i]]] + x
@@ -55,7 +54,6 @@ cat(paste0(colnames[i], ': ', x, '\n'))
 meanrho = meanrho / samplesize
 grandmeanrho = grandmeanrho + meanrho
 SEM = meanrho / sqrt(samplesize)
-grandSEM = grandSEM + SEM
 cat(paste0('Mean: ', meanrho, '\n'))
 cat(paste0('SEM: ', SEM, '\n'))
 cat('\n')
@@ -71,6 +69,10 @@ cat('===========================================================================
 for (s in subjs) {
     cat(paste0('boldMean.', s, ': ', grandx[[s]], '\n'))
 }
+grandmeanrho = grandmeanrho / nregions
+grandSEM = grandmeanrho / sqrt(nregions)
+cat(paste0('Mean: ', grandmeanrho, '\n'))
+cat(paste0('SEM: ', grandSEM, '\n'))
 cat('\n')
 
 cat('====================================================\n')
@@ -78,7 +80,7 @@ cat('Between-region BOLD response correlations by subject\n')
 cat('====================================================\n\n')
 
 for (s in subjs) {
-    print(cor(dfw[paste0('bold',rois,'.',s)]))
+    print(cor(dfw[paste0(rois,'.',s)]))
     cat('\n')
 }
 
@@ -86,7 +88,7 @@ cat('=========================================\n')
 cat('Between-region BOLD response correlations\n')
 cat('=========================================\n\n')
 
-print(cor(df[paste0('bold', rois)]))
+print(cor(df[paste0(rois)]))
 cat('\n')
 
 cat('==================================================================\n')
@@ -94,8 +96,8 @@ cat('By-region BOLD response correlation with mean of remaining regions\n')
 cat('==================================================================\n\n')
 
 for (i in 1:length(rois)) {
-    x = as.numeric(cor(df[paste0('bold', rois[i])], rowMeans(df[paste0('bold', rois[-i])])))
-    cat(paste0('bold', rois[i], ': ', x, '\n'))
+    x = as.numeric(cor(df[paste0(rois[i])], rowMeans(df[paste0(rois[-i])])))
+    cat(paste0(rois[i], ': ', x, '\n'))
 }
 cat('\n')
 
