cdb3701b85cd54274516f5a3c5fe3c1f7ef70374
schuler.77 <schuler.77@deasy.ling.ohio-state.edu>
2017-09-04 23:34:46 -0400

    linetrees2ymodel annotates linetrees with types

--- resource-incrsem/scripts/linetrees2ymodel.py
+++ resource-incrsem/scripts/linetrees2ymodel.py
@@ -1,25 +1,47 @@
 import sys
 import os
+import re
 import numpy
 import random
+import collections
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..', 'resource-gcg', 'scripts'))
 import tree
 import gcgtree
 import semcuegraph
 
+numpy.set_printoptions(linewidth=200)
+
+################################################################################
+
+def numberTerminals( t, n=0 ):
+  if len(t.ch)==0:
+    n += 1
+    t.n = n
+  for st in t.ch:
+    n = numberTerminals( st, n )
+  return n
 
 ################################################################################
 
 def factorConj( t ):
   if   len(t.ch)==0:                         return [ (1.0,t) ]
-  elif len(t.ch)==1:                         return [ (n,tree.Tree(t.c,[st])) for n,st in factorConj(t.ch[0]) ]
+  elif len(t.ch)==1:                         return [ (p,tree.Tree(t.c,[st])) for p,st in factorConj(t.ch[0]) ]
   elif len(t.ch)==2 and '-lC' in t.ch[0].c:
                                              lt = factorConj( t.ch[0] ) + factorConj( t.ch[1] )
-                                             tot = sum([n for n,st in lt]) if '-c' not in t.c else 1.0
-                                             return [ (n/tot,tree.Tree(t.c,st.ch)) for n,st in lt ]
+                                             tot = sum([p for p,st in lt]) if '-c' not in t.c else 1.0
+                                             return [ (p/tot,tree.Tree(t.c,st.ch)) for p,st in lt ]
   elif len(t.ch)==2 and '-c' in t.c:         return factorConj( t.ch[1] )
-  elif len(t.ch)==2:                         return [ (n0*n1,tree.Tree(t.c,[t0,t1])) for n0,t0 in factorConj(t.ch[0]) for n1,t1 in factorConj(t.ch[1]) ]
+  elif len(t.ch)==2:                         return [ (p0*p1,tree.Tree(t.c,[t0,t1])) for p0,t0 in factorConj(t.ch[0]) for p1,t1 in factorConj(t.ch[1]) ]
 
+################################################################################
+
+def mapFactoredToOrig( t, N2N, ctr=0 ):
+  if len(t.ch)==0:
+    ctr += 1
+    N2N[ctr] = t.n
+  for st in t.ch:
+    ctr = mapFactoredToOrig( st, N2N, ctr )
+  return ctr
 
 ################################################################################
 
@@ -27,11 +49,12 @@ def makeTraversal( G, Marked, m='', z='' ):
   if z=='':
     Marked = { }
     if G!={}: z = max( [ x for x,l in G ] )
-  Marked[ z ] = 1
+  if m!='0': Marked[ z ] = 1
+#  print( 'i am doing ' + m + ' ' + z, Marked )
   return tree.Tree( m+'|'+z,   [ makeTraversal( G, Marked, '-'+l, x      ) for x,l in G if G[x,l]==z and x not in Marked and l!='0' ]      ## incoming deps 
                              + [ makeTraversal( G, Marked, l,     G[x,l] ) for x,l in G if x==z and G[x,l] not in Marked            ] )    ## outgoing deps
 
-
+################################################################################
 ################################################################################
 
 KINTS = { }
@@ -55,94 +78,136 @@ def setKL( t, KINTS ):
 def calcBackward( t, L, M, N ):                ## bottom-up likelihood calculation
   t.u = numpy.ones((Y,1))
   for st in t.ch:
+    if st.c[0]!='0': calcBackward( st, L, M, N )
     l = int( st.c.partition('|')[0] ) if st.c[0]!='|' else 0
-    calcBackward( st, L, M, N )
-    if st.c[0]!='0':
-      t.u = numpy.multiply( t.u, M[:,:,[L+l]].reshape((Y,Y)).dot( st.u )  )
+    if st.c[0]!='0': t.u = numpy.multiply( t.u, M[[L+l],:,:].reshape((Y,Y)).dot( st.u )  )
     else:            t.u = numpy.multiply( t.u, N[:,[uniqInt(st.c)]] )
 
 ################################################################################
 
 def sampleTypes( t, L, M, N, yAbove=0 ):        ## top-down sampling
   l = int( t.c.partition('|')[0] ) if t.c[0]!='|' else 0
-  v = M[[yAbove],:,[L+l]]
-  post = numpy.multiply( v, t.u.T ).reshape(Y)
+  post = numpy.multiply( M[[L+l],[yAbove],:], t.u.T ).reshape(Y)
   t.y = numpy.random.choice( Y, p=post/post.sum() )
   for st in t.ch:
-    sampleTypes( st, L, M, N, t.y )
+    if st.c[0]!='0': sampleTypes( st, L, M, N, t.y )
 
 ################################################################################
 
 def addToCount( p, t, L, Mcount, Ncount, yAbove=0 ):
-  l = int( t.c.partition('|')[0] ) if t.c[0]!='|' else 0    #int(t.c[:2] if t.c[0]=='-' else t.c[0])
+  l = int( t.c.partition('|')[0] ) if t.c[0]!='|' else 0
   if t.c[0]=='0': Ncount[yAbove,uniqInt(t.c)] += p
   else:
-    Mcount[yAbove,t.y   ,L+l] += p
-    Mcount[t.y   ,yAbove,L-l] += p
+    Mcount[L+l,yAbove,t.y   ] += p
+    Mcount[L-l,t.y   ,yAbove] += p
+    for st in t.ch:
+      addToCount( p, st, L, Mcount, Ncount, t.y )
+
+################################################################################
+################################################################################
+
+def averageTypeOutcomes( p, t, Ymap, N2N, src='00', srcy=-1 ):
+  if t.c[0]=='0': Ymap[ N2N[int(src[:2])] ][ '-x%:' + t.c[2:] + '|%y' + str(srcy) ] += p
   for st in t.ch:
-    addToCount( p, st, L, Mcount, Ncount, t.y )
+    averageTypeOutcomes( p, st, Ymap, N2N, re.sub('.*[|]','',t.c), t.y )
 
 ################################################################################
 
-I = 100     ## number of iterations
-Y = 7     ## number of types
+def annotY( t, Ymap, ctr=0 ):
+  if len(t.ch)==1 and len(t.ch[0].ch)==0:
+    ctr += 1
+#    print( ctr, Ymap )
+    if ctr in Ymap:
+      p,xy = max( [ (Ymap[ctr][y],y) for y in Ymap[ctr] ] )
+      t.c += xy
+  for st in t.ch:
+    ctr = annotY( st, Ymap, ctr )
+  return ctr
+
+################################################################################
+################################################################################
+
+I = 10 #100    ## number of iterations
+Y = 5 #50    ## number of types
 L = 5       ## number of dep labels (arg positions)
-beta = 0.2  ## pseudocount mass
+alpha = 0.1
+beta  = 0.1  ## pseudocount mass
 
 ## read in corpus and convert to traversal trees...
-lptrav = [ ]
+lt       = [ ]
+llpttrav = [ ]
 for line in sys.stdin:
   Marked = { }
-  lpt = factorConj( gcgtree.GCGTree(line) ) 
-  lptrav += [ (p,makeTraversal(semcuegraph.SimpleCueGraph(semcuegraph.SemCueGraph(t)),Marked)) for p,t in lpt ]
+  lt.append( gcgtree.GCGTree(line) )
+  numberTerminals( lt[-1] )
+  lptFactored = factorConj( lt[-1] )
+  llpttrav.append( [ (p,t,makeTraversal(semcuegraph.SimpleCueGraph(semcuegraph.SemCueGraph(t)),Marked)) for p,t in lptFactored ] )
 
 ## set size params...
 K,L = 0,0
-for p,t in lptrav:
-  K,l = setKL( t, KINTS )
-  L = max(L,l)
+for lpttrav in llpttrav:
+  for p,t,trav in lpttrav:
+    K,l = setKL( trav, KINTS )
+    L = max(L,l)
 print( str(K) + ' predicate constants, ' + str(L) + ' dependency labels.' )
+
+## init models...
+M, N = numpy.zeros((L*2,Y,Y)), numpy.zeros((Y,K))
+C, D = numpy.zeros((L*2,Y,Y)), numpy.zeros((Y,K))
 
 ## gibbs sampling iterations...
 for i in range( I ):
+
+  ## draw model from dirichlet given counts and pseudocounts...
+  for l in range( 2*L ):
+    for y in range( Y ):
+      M[[l],[y],:] = numpy.random.dirichlet( C[l,y,:] + alpha )
+  for y in range( Y ):
+    N[[y],:] = numpy.random.dirichlet( D[y,:] + beta )
+
+  ## init counts...
+  C.fill( 0.0 )
+  D.fill( 0.0 )
   logprob = 0.0
 
-  ## init counts, models...
-  C, D = numpy.zeros((Y,Y,L*2)), numpy.zeros((Y,K))
-  M, N = numpy.zeros((Y,Y,L*2)), numpy.zeros((Y,K))
-
   ## iterate over corpus...
-  for p,t in lptrav:
+  for lpttrav in llpttrav:
+    for p,t,trav in lpttrav:
 
-    ## draw model from dirichlet given counts and pseudocounts...
-    for l in range( 2*L ):
-      for y in range( Y ):
-        M[[y],:,[l]] = numpy.random.dirichlet( C[y,:,l] + beta )
-    for y in range( Y ):
-      N[[y],:]     = numpy.random.dirichlet( D[y,:]   + beta )
+      ## draw types from model and observations...
+      calcBackward ( trav, L, M, N )   ## bot-up
+      sampleTypes  ( trav, L, M, N )   ## top-dn
 
-    ## draw types from model and observations...
-    calcBackward ( t, L, M, N )   ## bot-up
-    sampleTypes  ( t, L, M, N )   ## top-dn
+      ## recalculate counts...
+      addToCount( p, trav, L, C, D )
 
-    ## recalculate counts...
-    C.fill(0.0)
-    D.fill(0.0)
-    addToCount( p, t, L, C, D )
-    print( C )
-    exit( 0 )
+      logprob += numpy.log( p * M[0,0,:].dot( trav.u ) )
 
-    logprob += numpy.log( p * M[0,:,0].dot( t.u ) )
-
-  print( 'iteration ' + str(i) + ' logprob: ' + str(logprob) )
+  sys.stderr.write( 'iteration ' + str(i) + ' logprob: ' + str(logprob) + '\n' )
 
 ## dump models...
 for l in range( 2*L ):
   for y in range( Y ):
     for z in range( Y ):
-      print( 'M ' + str(l) + ' ' + str(y) + ' : ' + str(z) + ' = ' + str(M[y,z,l]) )
-for l in range( 2*L ):
-  for y in range( Y ):
-    for k in KINTS:
-      print( 'K ' + str(y) + ' : ' + k + ' = ' + str(N[y,KINTS[k]]) )
+      print( 'M ' + str(l) + ' ' + str(y) + ' : ' + str(z) + ' = ' + str(M[l,y,z]) )
+for y in range( Y ):
+  for k in KINTS:
+    print( 'K ' + str(y) + ' : ' + k + ' = ' + str(N[y,KINTS[k]]) )
+
+## print types on trees...
+for i,lpttrav in enumerate( llpttrav ):
+
+  Ymap = collections.defaultdict( lambda : collections.defaultdict(float) )
+  for p,t,trav in lpttrav:
+    N2N = { }
+    mapFactoredToOrig( t, N2N )
+#    print( trav )
+#    print( N2N )
+    averageTypeOutcomes( p, trav, Ymap, N2N )
+#    print( Ymap )
+
+#  print( semcuegraph.SemCueGraph(lt[i]) )
+  annotY( lt[i], Ymap )
+  print( str(lt[i]) )
+
 
