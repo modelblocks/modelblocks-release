fbefab75515b9e3940ecf990e783d0d353f21090
schuler.77 <schuler.77@deasy.ling.ohio-state.edu>
2018-08-07 00:07:18 -0400

    make semproc output -lA and -lM tags (not -lC or -lU unfortunately), which can (partially) reconstruct cuegraphs

--- resource-incrsem/scripts/tokdecs2linetrees.py
+++ resource-incrsem/scripts/tokdecs2linetrees.py
@@ -2,11 +2,18 @@ import os, sys, re
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..', 'resource-gcg', 'scripts'))
 from tree import Tree
 
-SS = [ ]
+SS = [ ]   ## derivation fragments (partial trees) derived at each storestate
+
+IDENTIFY_EXTRACTIONS = False
 
 def attachB(t,lu):
   if len(t.ch)>1: attachB(t.ch[1],lu)
   else:           t.ch = lu
+  return t
+
+def annotB(t,lu):
+  if len(t.ch)>1: annotB(t.ch[1],lu)
+  else:           t.c += lu
   return t
 
 for s in sys.stdin:
@@ -19,6 +26,15 @@ for s in sys.stdin:
     if m is not None:
         w,p,f,j,q = m.groups()
 
+        ## remove berk stuff (not needed after berk removed)...
+        p = re.sub( '_[0-9]+', '', p )
+
+        ## annotate operator tags if semproc...
+        if len(f.split('&'))>1:
+          bF,eF,_ = f.split('&')
+          if eF!='' and bF=='f1' and IDENTIFY_EXTRACTIONS: p            += '-l' + eF
+          if eF!='' and bF=='f0' and IDENTIFY_EXTRACTIONS: annotB( SS[-1], '-l' + eF )
+
         if f=='1' or f.startswith('f1'): SS.append( Tree(p,[Tree(w)]) )
         else:                            attachB( SS[-1], [ Tree(p,[Tree(w)]) ] )
 
@@ -30,7 +46,19 @@ for s in sys.stdin:
         else:
             a,b = re.split( '/' , re.sub( '\[[^\]]*\]:', '', q ) )   # get a,b from last q
 
+            ## remove berk stuff (not needed after berk removed)...
+            a = re.sub( '_[0-9]+', '', a )
+            b = re.sub( '_[0-9]+', '', b )
+
             # print( w, p, f, j, a, b )
+
+            ## annotate operator tags if semproc...
+            if len(j.split('&'))>1:
+              bJ,eJ,oL,oR = j.split('&')
+              if eJ!='' and bJ=='j1' and IDENTIFY_EXTRACTIONS: annotB( SS[-2], '-l' + eJ )
+              if eJ!='' and bJ=='j0' and IDENTIFY_EXTRACTIONS: a            += '-l' + eJ
+              if oL!='I' and oL!='m' or oR=='m'              : SS[-1].c     += '-l' + ('A' if oL>='1' and oL<='9' else 'A' if oR=='m' else oL)
+              if oR!='I' and oR!='m' or oL=='m'              : b            += '-l' + ('A' if oR>='1' and oR<='9' else 'A' if oL=='m' else oR)
 
             if j=='1' or j.startswith('j1'): SS = SS[:-2] + [ attachB( SS[-2], [ SS[-1], Tree(b) ] ) ]
             else:                            SS = SS[:-1] + [ Tree( a, [ SS[-1], Tree(b) ] ) ]
