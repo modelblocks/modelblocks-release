a8e79dc8a324c0d4bf9e39f2572dac3611953df8
schuler.77 <schuler.77@deasy.ling.ohio-state.edu>
2019-06-29 16:48:34 -0400

    add file senttrees2discgraphs.py

--- resource-gcg/scripts/senttrees2discgraphs.py
+++ resource-gcg/scripts/senttrees2discgraphs.py
@@ -19,39 +19,33 @@
 
 import sys, os, re, collections
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..', 'resource-gcg', 'scripts'))
+import tree
 import gcgtree
+import semcuegraph
+
+for a in sys.argv:
+  if a=='-d':
+    gcgtree.VERBOSE = True
+    semcuegraph.VERBOSE = True
 
 ################################################################################
 
-class CueGraph( dict ):
+discctr = 0
+sentctr = 0
+G       = semcuegraph.SemCueGraph( )
 
-  def __init__( G, s ):
-    G = { }
-    for dep in s.split():
-      x,l,y = re.match('([^,]*),([^,]*),(.*)',dep).groups()
-      G[x,l]=y
+for line in sys.stdin:
 
-  def __str__( G ):
-    return ' '.join( [ ','.join( (x,l,G[x,l]) ) for x,l in sorted(G) ] )
+  if '!ARTICLE' in line:
+    if discctr>0: print( str(G) )
+    sentctr = 0
+    G = semcuegraph.SemCueGraph( )
+    discctr += 1
 
-  def rename( G, xNew, xOld ):
-    if xOld != xNew:
-      for z,l in G.keys():
-        if G[z,l] == xOld: G[z,l] = xNew     ## replace old destination with new
-        if z == xOld:                        ## replace old source with new
-          if (xNew,l) not in G:
-            G[xNew,l] = G[xOld,l]
-            del G[xOld,l]
-      for z,l in G.keys():
-        if z == xOld and (xNew,l) in G: G.rename( G[xNew,l], G[xOld,l] )
+  else:
+    t = gcgtree.GCGTree( line )
+    G.add( t, ('0' if sentctr<10 else '') + str(sentctr) )
+    sentctr += 1
 
-  def result( G, l, x ):                     ## (f_l x)
-    if (x,l) not in G:  G[x,l] = x+l         ## if dest is new, name it after source and label
-    return G[x,l]
-
-  def equate( G, y, l, x ):                  ## y = (f_l x)
-    if (x,l) in G: G.rename( y, G[x,l] )     ## if source and label exist, rename
-    else:          G[x,l] = y                ## otherwise, add to dict
-
-
+print( str(G) )
 
