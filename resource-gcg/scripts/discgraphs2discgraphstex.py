9089737809d5933bdd1b29928487fa07aef314d0
schuler.77 <schuler.77@deasy.ling.ohio-state.edu>
2019-08-22 22:20:06 -0400

    obtain %.discgraph.pdf using tikz

---
+++ resource-gcg/scripts/discgraphs2discgraphstex.py
@@ -0,0 +1,64 @@
+import sys
+import sets
+
+## For each discourse graph...
+for line in sys.stdin:
+
+  line = line.rstrip()
+
+#  print( 'digraph G {' )   ## dot
+  print( '\\documentclass[tikz]{standalone}' )
+  print( '\\tikzset{>=latex,->}' )
+  print( '\\tikzstyle{reft}=[circle,minimum width=2.5ex,inner sep=0pt,draw]' )
+  print( '\\tikzstyle{pred}=[node distance=20mm,anchor=mid]' )
+  print( '\\begin{document}' )
+  print( '\\begin{tikzpicture}[scale=3]' )
+
+  Nodes = sets.Set()
+
+  ## For each assoc...
+  for assoc in sorted( line.split(' ') ):
+    src,lbl,dst = assoc.split(',')
+ 
+#    print( '"' + src + '" -> "' + dst + '" [label="' + lbl + '", color=' + ('black' if lbl in '0123456789' else 'magenta' if lbl=='s' else 'blue') + '];' )   #, constraint=false];' )  ## dot
+
+    for x in [src] if lbl=='0' else [src,dst]:
+      if x not in Nodes:
+        Nodes.add( x )
+        if   x[4]=='a':    print( '\\node(x' + x + ') at (' + x[2:4] + ('.2' if x[-1]=='r' else '.4') + ',' + x[0:2] + '.8) [reft]{' + x[4:] + '};' )
+        elif x[4]=='b':    print( '\\node(x' + x + ') at (' + x[2:4] + ('.2' if x[-1]=='r' else '.4') + ',' + x[0:2] + '.6) [reft]{' + x[4:] + '};' )
+        elif x[4]=='u':    print( '\\node(x' + x + ') at (' + x[2:4] + ('.2' if x[-1]=='r' else '.4') + ',' + x[0:2] + '.6) [reft]{' + x[4:] + '};' )
+        elif x[4:]=='s':   print( '\\node(x' + x + ') at (' + x[2:4] + '.8,' + x[0:2] + '.4) [reft]{' + x[4:] + '};\n' +
+                                  '\\node(w' + x + ')[above of=x' + x + ',pred]{' + x[0:4] + '};\n' +
+                                  '\\draw(w' + x + ') -- node[left]{S} (x' + x + ');' )
+        elif x[4:]=='r':   print( '\\node(x' + x + ') at (' + x[2:4] + '.6,' + x[0:2] + '.4) [reft]{' + x[4:] + '};' )
+        elif x[4:]=='sE':  print( '\\node(x' + x + ') at (' + x[2:4] + '.4,' + x[0:2] + '.4) [reft]{' + x[4:] + '};' )
+        elif x[4:]=='sEr': print( '\\node(x' + x + ') at (' + x[2:4] + '.2,' + x[0:2] + '.4) [reft]{' + x[4:] + '};' )
+        elif x[4:]=='sQ':  print( '\\node(x' + x + ') at (' + x[2:4] + '.6,' + x[0:2] + '.2) [reft]{' + x[4:] + '};' )
+        elif x[4:]=='sQr': print( '\\node(x' + x + ') at (' + x[2:4] + '.4,' + x[0:2] + '.2) [reft]{' + x[4:] + '};' )
+        elif x[4:]=='sR':  print( '\\node(x' + x + ') at (' + x[2:4] + '.8,' + x[0:2] + '.0) [reft]{' + x[4:] + '};' )
+        elif x[4:]=='sRr': print( '\\node(x' + x + ') at (' + x[2:4] + '.6,' + x[0:2] + '.0) [reft]{' + x[4:] + '};' )
+        else:              print( '\\node(x' + x + ') at (' + x[2:4] + '.0,' + x[0:2] + '.4) [reft]{' + x[4:] + '};' )
+
+    if lbl == '0':
+      print( '\\node(k' + src[0:4] + ')[below of=x' + src + ',pred] {' + dst + '};' )
+      print( '\\draw(x' + src + ') -- node[left]{0} (k' + src[0:4] + ');' )
+    elif lbl == 's':
+      print( '\\draw[magenta](x' + src + ') to [bend ' + ('left' if src[0:4]<dst[0:4] else 'right') + ',near start] node[above]{' + lbl + '} (x' + dst + ');' )
+#    elif lbl == '1' and src[0:4]==dst[0:4]:
+#      print( '\\draw(x' + src + ') -- node[below]{' + lbl + '} (x' + dst + ');' )
+    elif lbl >= '1' and lbl <= '9':
+      print( '\\draw(x' + src + ') to [bend ' + ('right' if src[0:4]<=dst[0:4] else 'left') + ',near start] node[below]{' + lbl + '} (x' + dst + ');' )
+    elif lbl == 'r' and src[0:4]==dst[0:4]:
+      print( '\\draw[blue](x' + src + ') -- node[below]{' + lbl + '} (x' + dst + ');' )
+    else:
+      print( '\\draw[blue](x' + src + ') to [bend ' + ('right' if src[0:4]<dst[0:4] else 'left') + ',near start] node[below]{' + lbl + '} (x' + dst + ');' )
+
+  print( '\\end{tikzpicture}' )
+  print( '\\end{document}' )  
+
+#  print( '{ ordering=out; x->"' + '"[style="invis"]; x->"'.join([ x for x in Nodes if ':' not in x ]) + '"[style="invis"]; }' )  ## dot
+#  print( '{ rank=same; "' + '"; "'.join(sorted([ x for x in Nodes if ':' in x ])) + '"; }' )   ## dot
+
+#  print( '}' )  ## dot
+
