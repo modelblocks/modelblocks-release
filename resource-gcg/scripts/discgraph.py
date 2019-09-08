84d89a63d4a918a2ac7721f6bc6982d2a367a824
schuler.77 <schuler.77@deasy.ling.ohio-state.edu>
2019-09-08 10:26:30 -0400

    error check scope arriving at smote redundant predicative node in discgraph.py

--- resource-gcg/scripts/discgraph.py
+++ resource-gcg/scripts/discgraph.py
@@ -167,6 +167,9 @@ class DiscGraph:
           if xLo in D.Scopes:
             sys.stderr.write( 'ERROR: scope should not be annotated on redundant predicative referent: ' + xLo + '\n' )
             print(           '#ERROR: scope should not be annotated on redundant predicative referent: ' + xLo )
+          if xLo in D.Scopes.values():
+            sys.stderr.write( 'ERROR: inheritance should not be annotated to redundant predicative referent: ' + xLo + '\n' )
+            print(           '#ERROR: inheritance should not be annotated to redundant predicative referent: ' + xLo )
           if xLo in D.Subs:
             sys.stderr.write( 'ERROR: inheritance should not be annotated from ' + str(D.Subs[xLo]) + ' to redundant predicative referent: ' + xLo + '\n' )
             print(           '#ERROR: inheritance should not be annotated from ' + str(D.Subs[xLo]) + ' to redundant predicative referent: ' + xLo )
