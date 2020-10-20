57a3013ae2c855dd970a0496efd86c3b41c2fcc9
Cory Shain <cory.shain@gmail.com>
2020-10-20 11:50:04 -0400

    Added missing CDR script

---
+++ resource-cdr/scripts/get_prereqs_from_baseform.py
@@ -0,0 +1,25 @@
+import sys
+import os
+import configparser
+
+config = configparser.ConfigParser()
+config.optionxform = str
+config.readfp(sys.stdin)
+
+prereqs = []
+
+if 'data' in config and 'X_train' in config['data']:
+    for x in config['data']['X_train'].split(';'):
+        if x not in prereqs:
+            prereqs.append(x)
+if 'data' in config and 'X_dev' in config['data']:
+    for x in config['data']['X_dev'].split(';'):
+        if x not in prereqs:
+            prereqs.append(x)
+if 'data' in config and 'X_test' in config['data']:
+    for x in config['data']['X_test'].split(';'):
+        if x not in prereqs:
+            prereqs.append(x)
+
+sys.stdout.write(' '.join(prereqs))
+
