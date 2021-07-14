3d69ae83a825042abbce6e40ad8cf9b8b7aef395
Christian Clark <clark.3664@osu.edu>
2021-07-14 17:11:41 -0400

    check performance on dev set during f model training

--- resource-incrsem/scripts/transformer/train_fmodel.py
+++ resource-incrsem/scripts/transformer/train_fmodel.py
@@ -36,7 +36,7 @@ def hvecIxReplace(hvec, hvec_to_ix):
     return new_hvec, top_count
 
 
-def prepare_data():
+def _initialize_finfo_list(infile):
     # list of lists; each outer list is an article
     per_article_finfo = list()
     
@@ -44,7 +44,7 @@ def prepare_data():
     curr_finfo = list()
     
     is_first_line = True
-    for line in sys.stdin:
+    for line in infile:
         is_new_article, depth, catb, hv_base, hv_filler, hv_ante, nulla, fdec = line.split()
         is_new_article = bool(int(is_new_article))
         if is_new_article:
@@ -65,9 +65,73 @@ def prepare_data():
         # constitutents (match and hvec)?
         finfo.fdec = fdec
         curr_finfo.append(finfo)
-        
 
     per_article_finfo.append(curr_finfo)
+
+    return per_article_finfo
+
+
+def _map_finfo_list_to_ix(per_article_finfo, catb_to_ix, fdecs_to_ix, hvb_to_ix, hvf_to_ix, hva_to_ix, dev=False):
+    '''
+    Replaces strings inside FInfo objects with their corresponding indices.
+    These modifications are done in place rather than creating new
+    Finfo objects.
+    '''
+
+    new_per_article_finfo = list()
+    for article_finfo in per_article_finfo:
+        new_article_finfo = list()
+        for finfo in article_finfo:
+            try:
+                finfo.catb = catb_to_ix[finfo.catb]
+                finfo.fdec = fdecs_to_ix[finfo.fdec]
+                finfo.hvb, finfo.hvb_top = hvecIxReplace(finfo.hvb, hvb_to_ix)
+                finfo.hvf, finfo.hvf_top = hvecIxReplace(finfo.hvf, hvf_to_ix)
+                finfo.hva, finfo.hva_top = hvecIxReplace(finfo.hva, hva_to_ix)
+            except KeyError:
+                # dev may contain fdecs, catbases, etc that haven't appeared in
+                # training data. Throw out any such data
+                if dev: continue
+                else: raise
+            new_article_finfo.append(finfo)
+        new_per_article_finfo.append(new_article_finfo)
+    return new_per_article_finfo
+
+
+def prepare_data():
+    per_article_finfo = _initialize_finfo_list(sys.stdin)
+
+#    # list of lists; each outer list is an article
+#    per_article_finfo = list()
+#    
+#    # list of f decisions for the current article
+#    curr_finfo = list()
+#    
+#    is_first_line = True
+#    for line in sys.stdin:
+#        is_new_article, depth, catb, hv_base, hv_filler, hv_ante, nulla, fdec = line.split()
+#        is_new_article = bool(int(is_new_article))
+#        if is_new_article:
+#            if is_first_line:
+#                is_first_line = False
+#            else:
+#                per_article_finfo.append(curr_finfo)
+#                curr_finfo = list()
+#
+#        finfo = FInfo()
+#        finfo.depth = int(depth)
+#        finfo.catb = catb
+#        finfo.hvb = extract_first_kvec(hv_base)
+#        finfo.hvf = extract_first_kvec(hv_filler)
+#        finfo.hva = extract_first_kvec(hv_ante)
+#        finfo.nulla = int(nulla)
+#        # TODO is there any downside to not splitting the fdec into its
+#        # constitutents (match and hvec)?
+#        finfo.fdec = fdec
+#        curr_finfo.append(finfo)
+#        
+#
+#    per_article_finfo.append(curr_finfo)
            
     all_hvb = set()
     all_hvf = set()
@@ -90,23 +154,32 @@ def prepare_data():
     hvf_to_ix = {hvec: i for i, hvec in enumerate(sorted(all_hvf))}
     hva_to_ix = {hvec: i for i, hvec in enumerate(sorted(all_hva))}
 
-    # replace strings with indices
-    for article in per_article_finfo:
-        for finfo in article:
-            finfo.catb = catb_to_ix[finfo.catb]
-            finfo.fdec = fdecs_to_ix[finfo.fdec]
-            finfo.hvb, finfo.hvb_top = hvecIxReplace(finfo.hvb, hvb_to_ix)
-            finfo.hvf, finfo.hvf_top = hvecIxReplace(finfo.hvf, hvf_to_ix)
-            finfo.hva, finfo.hva_top = hvecIxReplace(finfo.hva, hva_to_ix)
+    per_article_finfo = _map_finfo_list_to_ix(per_article_finfo, catb_to_ix,
+        fdecs_to_ix, hvb_to_ix, hvf_to_ix, hva_to_ix)
+#    # replace strings with indices
+#    for article in per_article_finfo:
+#        for finfo in article:
+#            finfo.catb = catb_to_ix[finfo.catb]
+#            finfo.fdec = fdecs_to_ix[finfo.fdec]
+#            finfo.hvb, finfo.hvb_top = hvecIxReplace(finfo.hvb, hvb_to_ix)
+#            finfo.hvf, finfo.hvf_top = hvecIxReplace(finfo.hvf, hvf_to_ix)
+#            finfo.hva, finfo.hva_top = hvecIxReplace(finfo.hva, hva_to_ix)
     return per_article_finfo, catb_to_ix, fdecs_to_ix, hvb_to_ix, hvf_to_ix, hva_to_ix
 
 
-def get_train_seqs(per_article_finfo, window_size):
-    train_seqs = list()
+def prepare_dev_data(dev_decpars, catb_to_ix, fdecs_to_ix, hvb_to_ix, hvf_to_ix, hva_to_ix):
+    per_article_finfo = _initialize_finfo_list(open(dev_decpars))
+    per_article_finfo = _map_finfo_list_to_ix(per_article_finfo, catb_to_ix,
+        fdecs_to_ix, hvb_to_ix, hvf_to_ix, hva_to_ix, dev=True)
+    return per_article_finfo
+
+
+def get_finfo_seqs(per_article_finfo, window_size):
+    finfo_seqs = list()
     for article in per_article_finfo:
         for i in range(0, len(article), window_size):
-            train_seqs.append(article[i:i+window_size])
-    return train_seqs
+            finfo_seqs.append(article[i:i+window_size])
+    return finfo_seqs
 
 
 def pad_target_matrix(targets, symbol):
@@ -123,7 +196,21 @@ def pad_target_matrix(targets, symbol):
 
 
 def train(f_config):
+    use_dev = f_config.getboolean('UseDev')
+    dev_decpars = f_config.get('DevFile')
+    use_gpu = f_config.getboolean('UseGPU')
+    window_size = f_config.getint('AttnWindowSize')
+    batch_size = f_config.getint('BatchSize')
+    epochs = f_config.getint('NEpochs')
+    l2_reg = f_config.getfloat('L2Reg')
+    learning_rate = f_config.getfloat('LearningRate')
+    if use_dev:
+        dev_all_finfo = prepare_dev_data(dev_decpars, catb_to_ix, fdecs_to_ix,
+             hvb_to_ix, hvf_to_ix, hva_to_ix)
+    weight_decay = f_config.getfloat('WeightDecay')
+
     per_article_finfo, catb_to_ix, fdecs_to_ix, hvb_to_ix, hvf_to_ix, hva_to_ix = prepare_data()
+
     model = TransformerFModel(
                 f_config=f_config,
                 catb_vocab_size=len(catb_to_ix),
@@ -133,31 +220,25 @@ def train(f_config):
                 output_dim=len(fdecs_to_ix)
     )
 
-    use_gpu = f_config.getboolean('UseGPU')
-    window_size = f_config.getint('AttnWindowSize')
-    batch_size = f_config.getint('BatchSize')
-    epochs = f_config.getint('NEpochs')
-    l2_reg = f_config.getfloat('L2Reg')
-    learning_rate = f_config.getfloat('LearningRate')
-    weight_decay = f_config.getfloat('WeightDecay')
-
     if use_gpu:
         model = model.cuda()
 
-    criterion = nn.NLLLoss()
+    #criterion = nn.NLLLoss()
     optimizer = optim.Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
-    train_seqs = get_train_seqs(per_article_finfo, window_size)
-    model.train()
+    train_seqs = get_finfo_seqs(per_article_finfo, window_size)
+
+    if use_dev:
+        dev_per_article_finfo = prepare_dev_data(dev_decpars, catb_to_ix, fdecs_to_ix,
+             hvb_to_ix, hvf_to_ix, hva_to_ix)
+        dev_seqs = get_finfo_seqs(dev_per_article_finfo, window_size)
 
     # TODO add validation on dev set
     for epoch in range(epochs):
+        model.train()
         c0 = time.time()
         random.shuffle(train_seqs)
         total_train_correct = 0
         total_train_loss = 0
-        total_dev_loss = 0
-        # TODO change these
-        dev_acc = 0
         total_train_items = 0
 
         for j in range(0, len(train_seqs), batch_size):
@@ -218,12 +299,55 @@ def train(f_config):
             if use_gpu:
                 class_weights = class_weights.to('cuda')
 
+            criterion = nn.NLLLoss(weight=class_weights)
             nll_loss = criterion(output, target)
             loss = nll_loss + l2_reg * l2_loss
             total_train_loss += loss.item()
             loss.backward()
             optimizer.step()
             optimizer.zero_grad()
+
+        if use_dev:
+            with torch.no_grad():
+                model.eval()
+
+                # L x N
+                dev_target = [[fi.fdec for fi in seq] for seq in dev_seqs]
+                dev_target = pad_target_matrix(dev_target, fdecs_to_ix[PAD])
+                if use_gpu:
+                    dev_target = dev_target.to('cuda')
+
+                # L x N x E
+                dev_output = model(dev_seqs)
+
+                _, dev_fdec = torch.max(dev_output.data, 2)
+
+                dev_correct = ((dev_fdec == dev_target) * (dev_target != fdecs_to_ix[PAD])).sum().item()
+
+                # NLLLoss requires target dimension to be N x L
+                # https://pytorch.org/docs/stable/generated/torch.nn.NLLLoss.html
+                dev_target = torch.transpose(dev_target, 0, 1) # LxN --> NxL
+                    
+                # NLLLoss requires output dimension to be N x E x L
+                dev_output = torch.transpose(dev_output, 0, 2) # LxNxE --> ExNxL
+                dev_output = torch.transpose(dev_output, 0, 1) # ExNxL --> NxExL
+
+                # assign PAD class 0 weight so that it doesn't influence loss.
+                # equally weight all other classes
+                num_classes = dev_output.shape[1]
+                # last class is PAD
+                assert fdecs_to_ix[PAD] ==_num_classes - 1
+                per_class_weight = 1/(num_classes-1)
+                class_weights = torch.FloatTensor([per_class_weight]*(num_classes-1) + [0])
+                if use_gpu:
+                    class_weights = class_weights.to('cuda')
+
+                criterion = nn.NLLLoss(weight=class_weights)
+                dev_loss = criterion(dev_output, dev_target).item()
+                dev_acc = 100 * (dev_correct / len(dev_all_finfo))
+        else:
+            dev_acc = 0
+            dev_loss = 0
 
         eprint('Epoch {:04d} | AvgTrainLoss {:.4f} | TrainAcc {:.4f} | DevLoss {:.4f} | DevAcc {:.4f} | Time {:.4f}'.
                format(epoch, total_train_loss / ((len(train_seqs) // batch_size) + 1), 100 * (total_train_correct / total_train_items),
