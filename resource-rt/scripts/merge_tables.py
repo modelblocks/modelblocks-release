37ba301d09372f8776fc80c77954a21dc2a333f5
schuler.77 <schuler.77@deasy.ling.ohio-state.edu>
2021-07-19 17:26:09 -0400

    fix merge tables

--- resource-rt/scripts/merge_tables.py
+++ resource-rt/scripts/merge_tables.py
@@ -6,9 +6,11 @@ argparser.add_argument('f1', type=str, nargs=1, help='Path to first input data t
 argparser.add_argument('f2', type=str, nargs=1, help='Path to second input data table')
 argparser.add_argument('key_cols', metavar='key', type=str, nargs='+', \
     help='Merge key fields')
-argparser.add_argument('-H', '--how', default='inner', help='Merge type (inner, outer, left, right, default: inner).')
+#argparser.add_argument('-H', '--how', default='inner', help='Merge type (inner, outer, left, right, default: inner).')
 argparser.add_argument('-f', '--fallback', type=int, default=1, help='Which data table to return in case of key match failure. Either 1 or 2.')
 args, unknown = argparser.parse_known_args()
+
+#merge_how = 'outer' if args.outer else 'inner'
 
 def main():
     data1 = pd.read_csv(args.f1[0],sep=' ',skipinitialspace=True)
@@ -25,7 +27,8 @@ def main():
     else:
         no_dups = [c for c in data2.columns.values if c not in data1.columns.values]
         data2_cols = args.key_cols + no_dups
-        merged = pd.merge(data1, data2.filter(items=data2_cols), how=merge_how, on=args.key_cols, suffixes=('', '_2'))
+#        merged = pd.merge(data1, data2.filter(items=data2_cols), how=merge_how, on=args.key_cols, suffixes=('', '_2'))
+        merged = pd.merge(data1, data2.filter(items=data2_cols), on=args.key_cols, suffixes=('', '_2'))
         merged = merged * 1 # convert boolean to [1,0]
 #        if 'subject' in merged.columns:
 #            merged.sort_values(['subject'] + args.key_cols, inplace=True)
