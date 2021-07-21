32f2b53dc8a969b9febde6c97c54dbb0ca2e45b2
Christian Clark <clark.3664@osu.edu>
2021-07-21 15:28:33 -0400

    python code for transformer J model. this ran with no errors for a toy input corpus

---
+++ resource-incrsem/scripts/transformer/train_jmodel.py
@@ -0,0 +1,393 @@
+import sys, configparser, torch, re, os, time, random, pickle
+from collections import Counter
+import torch.nn as nn
+import torch.optim as optim
+
+from train_fmodel import eprint, extract_first_kvec, hvecIxReplace
+from transformerjmodel import TransformerJModel
+
+# The C++ code expects a certain format for each jdec, so this is
+# a fake jdec used for padding
+PAD = '0&J&J&J'
+
+
+# Dummy class for recording information about each J decision
+class JInfo:
+    pass
+
+def _initialize_jinfo_list(infile):
+    # list of lists; each outer list is a sentence
+    per_sentence_jinfo = list()
+    
+    # list of f decisions for the current sentence
+    curr_jinfo = list()
+    
+    is_first_line = True
+    for line in infile:
+        is_new_sentence, depth, cat_anc, hv_anc, hv_filler, cat_lc, hv_lc, \
+            jdec = line.split()
+        is_new_sentence = bool(int(is_new_sentence))
+        if is_new_sentence:
+            if is_first_line:
+                is_first_line = False
+            else:
+                per_sentence_jinfo.append(curr_jinfo)
+                curr_jinfo = list()
+
+        jinfo = JInfo()
+        jinfo.depth = int(depth)
+        jinfo.cat_anc = cat_anc
+        jinfo.hv_anc = extract_first_kvec(hv_anc)
+        jinfo.hv_filler = extract_first_kvec(hv_filler)
+        jinfo.cat_lc = cat_lc
+        jinfo.hv_lc = extract_first_kvec(hv_lc)
+        jinfo.jdec = jdec
+        curr_jinfo.append(jinfo)
+
+    per_sentence_jinfo.append(curr_jinfo)
+
+    return per_sentence_jinfo
+
+
+def _map_jinfo_list_to_ix(per_sentence_jinfo, cat_anc_to_ix, hv_anc_to_ix,
+    hv_filler_to_ix, cat_lc_to_ix, hv_lc_to_ix, jdecs_to_ix, dev=False):
+    '''
+    Replaces strings inside JInfo objects with their corresponding indices.
+    These modifications are done in place rather than creating new
+    Jinfo objects.
+    '''
+
+    new_per_sentence_jinfo = list()
+    for sentence_jinfo in per_sentence_jinfo:
+        new_sentence_jinfo = list()
+        for jinfo in sentence_jinfo:
+            try:
+                jinfo.cat_anc = cat_anc_to_ix[jinfo.cat_anc]
+                jinfo.hv_anc = hvecIxReplace(jinfo.hv_anc, hv_anc_to_ix)
+                jinfo.hv_filler = hvecIxReplace(jinfo.hv_filler, hv_filler_to_ix)
+                jinfo.cat_lc = cat_lc_to_ix[jinfo.cat_lc]
+                jinfo.hv_lc = hvecIxReplace(jinfo.hv_lc, hv_lc_to_ix)
+                jinfo.jdec = jdecs_to_ix[jinfo.jdec]
+            except KeyError:
+                # dev may contain jdecs, cats, etc that haven't appeared in
+                # training data. Throw out any such data
+                if dev: continue
+                else: raise
+            new_sentence_jinfo.append(jinfo)
+        new_per_sentence_jinfo.append(new_sentence_jinfo)
+    return new_per_sentence_jinfo
+
+
+def prepare_data():
+    per_sentence_jinfo = _initialize_jinfo_list(sys.stdin)
+           
+    all_cat_anc = set()
+    all_hv_anc = set()
+    all_hv_filler = set()
+    all_cat_lc = set()
+    all_hv_lc = set()
+    all_jdecs = set()
+    for sentence in per_sentence_jinfo:
+        for jinfo in sentence:
+            all_cat_anc.add(jinfo.cat_anc)
+            all_hv_anc.update(set(jinfo.hv_anc))
+            all_hv_filler.update(set(jinfo.hv_filler))
+            all_cat_lc.add(jinfo.cat_lc)
+            all_hv_lc.update(jinfo.hv_lc)
+            all_jdecs.add(jinfo.jdec)
+
+    cat_anc_to_ix = {cat: i for i, cat in enumerate(sorted(all_cat_anc))}
+    hv_anc_to_ix = {hvec: i for i, hvec in enumerate(sorted(all_hv_anc))}
+    hv_filler_to_ix = {hvec: i for i, hvec in enumerate(sorted(all_hv_filler))}
+    cat_lc_to_ix = {cat: i for i, cat in enumerate(sorted(all_cat_lc))}
+    hv_lc_to_ix = {hvec: i for i, hvec in enumerate(sorted(all_hv_lc))}
+    jdecs_to_ix = {jdec: i for i, jdec in enumerate(sorted(all_jdecs))}
+    # ID for padding symbol added at end of sequence
+    jdecs_to_ix[PAD] = len(jdecs_to_ix)
+
+    per_sentence_jinfo = _map_jinfo_list_to_ix(
+        per_sentence_jinfo, cat_anc_to_ix, hv_anc_to_ix, hv_filler_to_ix,
+        cat_lc_to_ix, hv_lc_to_ix, jdecs_to_ix
+    )
+
+    return per_sentence_jinfo, cat_anc_to_ix, hv_anc_to_ix, hv_filler_to_ix, \
+        cat_lc_to_ix, hv_lc_to_ix, jdecs_to_ix
+
+
+def prepare_dev_data(dev_decpars, cat_anc_to_ix, hv_anc_to_ix, hv_filler_to_ix,
+    cat_lc_to_ix, hv_lc_to_ix, jdecs_to_ix):
+    per_sentence_jinfo = _initialize_jinfo_list(open(dev_decpars))
+    per_sentence_jinfo = _map_jinfo_list_to_ix(
+        per_sentence_jinfo, cat_anc_to_ix, hv_anc_to_ix, hv_filler_to_ix,
+        cat_lc_to_ix, hv_lc_to_ix, jdecs_to_ix, dev=True
+    )
+    return per_sentence_jinfo
+
+
+def get_jinfo_seqs(per_sentence_jinfo, window_size):
+    jinfo_seqs = list()
+    for sentence in per_sentence_jinfo:
+        for i in range(0, len(sentence), window_size):
+            jinfo_seqs.append(sentence[i:i+window_size])
+    return jinfo_seqs
+
+
+def pad_target_matrix(targets, symbol):
+    '''
+    Given a list of per-sequence JDec targets and a padding symbol,
+    returns an SxN padded matrix of targets.
+    S is max sequence length, and N in number of sequences
+    '''
+    max_seq_length = max(len(t) for t in targets)
+    padded_targets = list()
+    for t in targets:
+        padded_targets.append(t + [symbol]*(max_seq_length - len(t)))
+    return torch.transpose(torch.LongTensor(padded_targets), 0, 1)
+
+
+def train(j_config):
+    use_dev = j_config.getboolean('UseDev')
+    dev_decpars = j_config.get('DevFile')
+    use_gpu = j_config.getboolean('UseGPU')
+    window_size = j_config.getint('AttnWindowSize')
+    batch_size = j_config.getint('BatchSize')
+    epochs = j_config.getint('NEpochs')
+    l2_reg = j_config.getfloat('L2Reg')
+    learning_rate = j_config.getfloat('LearningRate')
+    weight_decay = j_config.getfloat('WeightDecay')
+
+    per_sentence_jinfo, cat_anc_to_ix, hv_anc_to_ix, hv_filler_to_ix, \
+        cat_lc_to_ix, hv_lc_to_ix, jdecs_to_ix = prepare_data()
+
+    model = TransformerJModel(
+                j_config=j_config,
+                cat_anc_vocab_size=len(cat_anc_to_ix),
+                hv_anc_vocab_size=len(hv_anc_to_ix),
+                hv_filler_vocab_size=len(hv_filler_to_ix),
+                cat_lc_vocab_size=len(cat_lc_to_ix),
+                hv_lc_vocab_size=len(hv_lc_to_ix),
+                output_dim=len(jdecs_to_ix)
+    )
+
+    if use_gpu:
+        model = model.cuda()
+
+    #criterion = nn.NLLLoss()
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
+    train_seqs = get_jinfo_seqs(per_sentence_jinfo, window_size)
+
+    if use_dev:
+        dev_per_sentence_jinfo = prepare_dev_data(
+            dev_decpars, cat_anc_to_ix, hv_anc_to_ix, hv_filler_to_ix,
+            cat_lc_to_ix, hv_lc_to_ix, jdecs_to_ix
+        )
+        dev_seqs = get_jinfo_seqs(dev_per_sentence_jinfo, window_size)
+
+    # TODO add validation on dev set
+    for epoch in range(epochs):
+        model.train()
+        c0 = time.time()
+        random.shuffle(train_seqs)
+        total_train_correct = 0
+        total_train_loss = 0
+        total_train_items = 0
+
+        for j in range(0, len(train_seqs), batch_size):
+            if use_gpu:
+                l2_loss = torch.cuda.FloatTensor([0])
+            else:
+                l2_loss = torch.FloatTensor([0])
+            for param in model.parameters():
+                if torch.numel(param) == 0:
+                    continue
+                l2_loss += torch.mean(param.pow(2))
+
+            batch = train_seqs[j:j+batch_size]
+            target = [[ji.jdec for ji in seq] for seq in batch]
+            # L x N
+            # Note: padding of the input sequence happens within the
+            # forward method of the J model
+            target = pad_target_matrix(target, jdecs_to_ix[PAD])
+            if use_gpu:
+                target = target.to('cuda')
+
+            # output dimension is L x N x E
+            # L: window length
+            # N: batch size
+            # E: output dimensionality (number of classes)
+            output = model(batch)
+
+            # jdec dimension is L x N
+            _, jdec = torch.max(output.data, 2)
+
+            # count all items where jdec matches target and target isn't padding
+            train_correct = ((jdec == target) * (target != jdecs_to_ix[PAD])).sum().item()
+            total_train_correct += train_correct
+            
+            # again, ignore padding
+            total_train_items += (target != jdecs_to_ix[PAD]).sum().item()
+            #total_train_items += target.numel()
+
+            
+            # NLLLoss requires target dimension to be N x L
+            # https://pytorch.org/docs/stable/generated/torch.nn.NLLLoss.html
+            target = torch.transpose(target, 0, 1) # LxN --> NxL
+                
+            # NLLLoss requires output dimension to be N x E x L
+            output = torch.transpose(output, 0, 2) # LxNxE --> ExNxL
+            output = torch.transpose(output, 0, 1) # ExNxL --> NxExL
+
+
+            # assign PAD class 0 weight so that it doesn't influence loss.
+            # equally weight all other classes
+            num_classes = output.shape[1]
+            # last class is PAD
+            assert jdecs_to_ix[PAD] == num_classes - 1
+            per_class_weight = 1/(num_classes-1)
+            class_weights = torch.FloatTensor([per_class_weight]*(num_classes-1) + [0])
+
+            if use_gpu:
+                class_weights = class_weights.to('cuda')
+
+            criterion = nn.NLLLoss(weight=class_weights)
+            nll_loss = criterion(output, target)
+            loss = nll_loss + l2_reg * l2_loss
+            total_train_loss += loss.item()
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+
+        if use_dev:
+            with torch.no_grad():
+                model.eval()
+
+                # L x N
+                dev_target = [[ji.jdec for ji in seq] for seq in dev_seqs]
+                dev_target = pad_target_matrix(dev_target, jdecs_to_ix[PAD])
+                if use_gpu:
+                    dev_target = dev_target.to('cuda')
+
+                # L x N x E
+                dev_output = model(dev_seqs)
+
+                _, dev_jdec = torch.max(dev_output.data, 2)
+
+                dev_correct = ((dev_jdec == dev_target) * (dev_target != jdecs_to_ix[PAD])).sum().item()
+                total_dev_items = (dev_target != jdecs_to_ix[PAD]).sum().item()
+
+                # NLLLoss requires target dimension to be N x L
+                # https://pytorch.org/docs/stable/generated/torch.nn.NLLLoss.html
+                dev_target = torch.transpose(dev_target, 0, 1) # LxN --> NxL
+                    
+                # NLLLoss requires output dimension to be N x E x L
+                dev_output = torch.transpose(dev_output, 0, 2) # LxNxE --> ExNxL
+                dev_output = torch.transpose(dev_output, 0, 1) # ExNxL --> NxExL
+
+                # assign PAD class 0 weight so that it doesn't influence loss.
+                # equally weight all other classes
+                num_classes = dev_output.shape[1]
+                # last class is PAD
+                assert jdecs_to_ix[PAD] == num_classes - 1
+                per_class_weight = 1/(num_classes-1)
+                class_weights = torch.FloatTensor([per_class_weight]*(num_classes-1) + [0])
+                if use_gpu:
+                    class_weights = class_weights.to('cuda')
+
+                criterion = nn.NLLLoss(weight=class_weights)
+                dev_loss = criterion(dev_output, dev_target).item()
+                dev_acc = 100 * (dev_correct / total_dev_items)
+        else:
+            dev_acc = 0
+            dev_loss = 0
+
+        eprint('Epoch {:04d} | AvgTrainLoss {:.4f} | TrainAcc {:.4f} | DevLoss {:.4f} | DevAcc {:.4f} | Time {:.4f}'.
+               format(epoch, total_train_loss / ((len(train_seqs) // batch_size) + 1), 100 * (total_train_correct / total_train_items),
+                      dev_loss, dev_acc, time.time() - c0))
+
+    return model, cat_anc_to_ix, hv_anc_to_ix, hv_filler_to_ix, \
+        cat_lc_to_ix, hv_lc_to_ix, jdecs_to_ix
+
+
+def main(config):
+    j_config = config['JModel']
+    torch.manual_seed(j_config.getint('Seed'))
+    save_pytorch = j_config.getboolean('SaveTorchModel')
+    pytorch_fn = j_config.get('TorchFilename')
+    extra_params_fn = j_config.get('ExtraParamsFilename')
+    model, cat_anc_to_ix, hv_anc_to_ix, hv_filler_to_ix, cat_lc_to_ix, \
+        hv_lc_to_ix, jdecs_to_ix = train(j_config)
+
+    model.eval()
+    params = [ 
+        ('pre_attn_fc.weight', 'J P'),
+        ('pre_attn_fc.bias', 'J p'),
+        ('attn.in_proj_weight', 'J I'),
+        ('attn.in_proj_bias', 'J i'),
+        ('attn.out_proj.weight', 'J O'),
+        ('attn.out_proj.bias', 'J o'),
+        ('fc1.weight', 'J F'),
+        ('fc1.bias', 'J f'),
+        ('fc2.weight', 'J S'),
+        ('fc2.bias', 'J s')
+    ]
+
+    for param, prefix in params:
+        if j_config.getboolean('UseGPU'):
+            weights = model.state_dict()[param].data.cpu().numpy()
+        else:
+            weights = model.state_dict()[param].data.numpy()
+        print(prefix, ','.join(map(str, weights.flatten('F').tolist())))
+
+    if not j_config.getboolean('AblateSyn'):
+        if j_config.getboolean('UseGPU'):
+            cat_anc_weights = model.state_dict()['cat_anc_embeds.weight'].data.cpu().numpy()
+            cat_lc_weights = model.state_dict()['cat_lc_embeds.weight'].data.cpu().numpy()
+        else:
+            cat_anc_weights = model.state_dict()['cat_anc_embeds.weight'].data.numpy()
+            cat_lc_weights = model.state_dict()['cat_lc_embeds.weight'].data.numpy()
+        for cat, ix in sorted(cat_anc_to_ix.items()):
+            print('C A ' + str(cat) + ' ' + ','.join(map(str, cat_anc_weights[ix])))
+        for cat, ix in sorted(cat_lc_to_ix.items()):
+            print('C L ' + str(cat) + ' ' + ','.join(map(str, cat_lc_weights[ix])))
+
+    if not j_config.getboolean('AblateSem'):
+        if j_config.getboolean('UseGPU'):
+            hv_anc_weights = model.state_dict()['hv_anc_embeds.weight'].data.cpu().numpy()
+            hv_filler_weights = model.state_dict()['hv_filler_embeds.weight'].data.cpu().numpy()
+            hv_lc_weights = model.state_dict()['hv_lc_embeds.weight'].data.cpu().numpy()
+        else:
+            hv_anc_weights = model.state_dict()['hv_anc_embeds.weight'].data.numpy()
+            hv_filler_weights = model.state_dict()['hv_filler_embeds.weight'].data.numpy()
+            hv_lc_weights = model.state_dict()['hv_lc_embeds.weight'].data.numpy()
+
+        for hvec, ix in sorted(hv_anc_to_ix.items()):
+            print('K A ' + str(hvec) + ' ' + ','.join(map(str, hv_anc_weights[ix])))
+        for hvec, ix in sorted(hv_filler_to_ix.items()):
+            print('K F ' + str(hvec) + ' ' + ','.join(map(str, hv_filler_weights[ix])))
+        for hvec, ix in sorted(hv_lc_to_ix.items()):
+            print('K L ' + str(hvec) + ' ' + ','.join(map(str, hv_lc_weights[ix])))
+
+    for jdec, ix in sorted(jdecs_to_ix.items()):
+        print('j ' + str(ix) + ' ' + str(jdec))
+
+    if save_pytorch:
+        torch.save(model.state_dict(), pytorch_fn)
+        # these are needed to initialize the model if we want to reload it
+        extra_params = {
+            'cat_anc_vocab_size': len(cat_anc_to_ix),
+            'hv_anc_vocab_size': len(hv_anc_to_ix),
+            'hv_filler_vocab_size': len(hv_filler_to_ix),
+            'cat_lc_vocab_size': len(cat_lc_to_ix),
+            'hv_lc_vocab_size': len(hv_lc_to_ix),
+            'output_dim': len(jdecs_to_ix)
+        }
+        pickle.dump(extra_params, open(extra_params_fn, 'wb'))
+
+
+if __name__ == '__main__':
+    config = configparser.ConfigParser(allow_no_value=True)
+    config.read(sys.argv[1])
+    for section in config:
+        eprint(section, dict(config[section]))
+    main(config)
+
---
+++ resource-incrsem/scripts/transformer/transformerjmodel.py
@@ -0,0 +1,252 @@
+import torch, math
+import torch.nn as nn
+import torch.nn.functional as F
+
+from transformerfmodel import PositionalEncoding
+from train_fmodel import eprint
+
+class TransformerJModel(nn.Module):
+    def __init__(self, j_config, cat_anc_vocab_size, hv_anc_vocab_size,
+                 hv_filler_vocab_size, cat_lc_vocab_size, hv_lc_vocab_size,
+                 output_dim):
+        super(TransformerJModel, self).__init__()
+        self.syn_dim = j_config.getint('SynDim')
+        self.sem_dim = j_config.getint('SemDim')
+        self.hidden_dim = j_config.getint('HiddenDim')
+        self.dropout_prob = j_config.getfloat('DropoutProb')
+        self.ablate_syn = j_config.getboolean('AblateSyn')
+        self.ablate_sem = j_config.getboolean('AblateSem')
+        self.use_positional_encoding = j_config.getboolean('UsePositionalEncoding')
+        # TODO this is int in the old config -- why?
+        self.use_gpu = j_config.getboolean('UseGPU')
+        # TODO add num_blocks option for multiple transformer blocks
+        self.num_heads = j_config.getint('NumHeads')
+        self.attn_dim = j_config.getint('AttnDim')
+        # TODO decide whether to use this at runtime or just for
+        # training
+        self.attn_window_size = j_config.getint('AttnWindowSize')
+        self.hv_anc_vocab_size = hv_anc_vocab_size
+        self.hv_filler_vocab_size = hv_filler_vocab_size
+        self.hv_lc_vocab_size = hv_lc_vocab_size
+        self.output_dim = output_dim
+        self.max_depth = 7
+
+        self.cat_anc_embeds = nn.Embedding(cat_anc_vocab_size, self.syn_dim)
+        self.cat_lc_embeds = nn.Embedding(cat_lc_vocab_size, self.syn_dim)
+        self.hv_anc_embeds = nn.Embedding(hv_anc_vocab_size, self.sem_dim)
+        self.hv_filler_embeds = nn.Embedding(hv_filler_vocab_size, self.sem_dim)
+        self.hv_lc_embeds = nn.Embedding(hv_lc_vocab_size, self.sem_dim)
+
+        # attn intput comprises:
+        # - depth (max_depth)
+        # - ancestor cat embedding (syn_dim)
+        # - ancestor hvec (sem_dim)
+        # - filler hvec (sem_dim)
+        # - left child cat (syn_dim)
+        # - left child hvec (sem_dim)
+        attn_input_dim = self.max_depth + 2*self.syn_dim + 3*self.sem_dim
+
+        # project the attention input to the right dimensionality
+        self.pre_attn_fc = nn.Linear(attn_input_dim, self.attn_dim, bias=True)
+
+        # this layer adds positional encodings to the output of pre_attn_fc
+        # its output is used for queries, keys, and values
+        self.positional_encoding = PositionalEncoding(self.attn_dim)
+
+        # TODO define this as a block so you can have multiple attention layers
+        self.attn =  nn.MultiheadAttention(
+            embed_dim=self.attn_dim,
+            num_heads=self.num_heads,
+            bias=True
+        )
+
+        # TODO make this resnet?
+        self.fc1 = nn.Linear(self.attn_dim, self.hidden_dim, bias=True)
+        self.dropout = nn.Dropout(self.dropout_prob)
+        self.relu = F.relu
+        self.fc2 = nn.Linear(self.hidden_dim, self.output_dim, bias=True)
+
+
+    def get_sparse_hv_matrix(self, hv, vocab_size):
+        rows = list()
+        cols = list()
+        for i, h in enumerate(hv):
+            for predcon in h:
+                rows.append(i)
+                cols.append(predcon)
+        values = [1 for i in range(len(rows))]
+        hv_sparse =  torch.sparse_coo_tensor(
+            [rows, cols], values, (len(hv), vocab_size), dtype=torch.float
+        )
+        if self.use_gpu:
+            hv_sparse = hv_sparse.to('cuda')
+        return hv_sparse
+
+    
+    def get_per_sequence_x(self, batch_jinfo):
+        per_sequence_x = list()
+        for seq in batch_jinfo:
+            if self.ablate_syn:
+                cat_anc_embed = torch.zeros(
+                    [len(seq), self.syn_dim],
+                    dtype=torch.float
+                )
+                cat_lc_embed = torch.zeros(
+                    [len(seq), self.syn_dim],
+                    dtype=torch.float
+                )
+            else:
+                cat_anc = [ji.cat_anc for ji in seq]
+                cat_lc = [ji.cat_lc for ji in seq]
+                cat_anc = torch.LongTensor(cat_anc)
+                cat_lc = torch.LongTensor(cat_lc)
+                if self.use_gpu:
+                    cat_anc = cat_anc.to('cuda')
+                    cat_lc = cat_lc.to('cuda')
+                cat_anc_embed = self.cat_anc_embeds(cat_anc)
+                cat_lc_embed = self.cat_lc_embeds(cat_lc)
+
+
+            # ancestor, filler, left child hvec embeddings
+            if self.ablate_sem:
+                hv_anc_embed = torch.zeros(
+                    [len(seq), self.sem_dim], dtype=torch.float
+                )
+                hv_filler_embed = torch.zeros(
+                    [len(seq), self.sem_dim], dtype=torch.float
+                )
+                hv_lc_embed = torch.zeros(
+                    [len(seq), self.ant_dim], dtype=torch.float
+                )
+
+            else:
+                hv_anc = [ji.hv_anc for ji in seq]
+                hv_anc_sparse = self.get_sparse_hv_matrix(
+                    hv_anc, self.hv_anc_vocab_size
+                )
+                hv_anc_embed = torch.sparse.mm(
+                    hv_anc_sparse, self.hv_anc_embeds.weight
+                )
+
+                hv_filler = [ji.hv_filler for ji in seq]
+                hv_filler_sparse = self.get_sparse_hv_matrix(
+                    hv_filler, self.hv_filler_vocab_size
+                )
+                hv_filler_embed = torch.sparse.mm(
+                    hv_filler_sparse, self.hv_filler_embeds.weight
+                )
+
+                hv_lc = [ji.hv_lc for ji in seq]
+                hv_lc_sparse = self.get_sparse_hv_matrix(
+                    hv_lc, self.hv_lc_vocab_size
+                )
+                hv_lc_embed = torch.sparse.mm(
+                    hv_lc_sparse, self.hv_lc_embeds.weight
+                )
+
+
+            depth = [ji.depth for ji in seq]
+            depth = F.one_hot(torch.LongTensor(depth), self.max_depth).float()
+
+            if self.use_gpu:
+                depth = depth.to('cuda')
+
+            seq_x = torch.cat(
+                (cat_anc_embed, hv_anc_embed, hv_filler_embed, 
+                 cat_lc_embed, hv_lc_embed, depth),
+                dim=1
+            ) 
+            per_sequence_x.append(seq_x)
+    
+        return per_sequence_x
+
+
+    def get_padded_input_matrix(self, per_sequence_x):
+        max_length = max(len(seq) for seq in per_sequence_x)
+        width = per_sequence_x[0].size()[1]
+        padded_seqs = list()
+        for seq_x in per_sequence_x:
+            seq_length = seq_x.size()[0]
+            padded_seq = torch.Tensor(max_length, width)
+            # -1 is used for padding
+            padded_seq.fill_(-1)
+            padded_seq[:seq_length, :] = seq_x
+            padded_seqs.append(padded_seq)
+        x = torch.stack(padded_seqs, dim=1)
+        if self.use_gpu:
+            x = x.to('cuda')
+        return x
+
+
+    def get_attn_mask(self, seq_length):
+        # entries marked as True are what we want to mask
+        mask = torch.ones(seq_length, seq_length, dtype=bool)
+        if self.use_gpu:
+            mask = mask.to('cuda')
+        return torch.triu(mask, diagonal=1)
+
+
+    # TODO implement this
+    def forward(self, batch_jinfo):
+        # list of matrices, one matrix for each sequence
+        per_sequence_x = self.get_per_sequence_x(batch_jinfo)
+
+        # attn_input is a 3D tensor of dimensionality SxNxE
+        # S: sequence length
+        # N: batch size (number of sequences)
+        # E: embedding size
+        attn_input = self.get_padded_input_matrix(per_sequence_x)
+        return self.compute(attn_input)
+
+
+    def compute(self, attn_input, verbose=False):
+        # the same matrix is used as query, key, and value. Within the attn
+        # layer this will be projected to a separate q, k, and v for each
+        # attn head
+#        if verbose:
+#            print('F final word attn input:')
+#            for x in attn_input[-1, 0]:
+#                print(x.item())
+#            weights = self.state_dict()['pre_attn_fc.weight'].data.cpu().numpy()
+#            print('F pre attn fc weights shape:', weights.shape)
+#            print('F pre attn fc weights numpy:')
+#            print(weights)
+#            print('F pre attn fc weights:')
+#            # F is column-major order
+#            for x in weights.flatten('F'):
+#                print(x)
+#            print('F final word pre_attn_fc bias:')
+#            bias = self.state_dict()['pre_attn_fc.bias'].data.cpu().numpy()
+#            for x in bias:
+#                print(x)
+        qkv = self.pre_attn_fc(attn_input)
+        if verbose:
+            print('F final word\'s qkv:')
+            for x in qkv[-1, 0]:
+                print(x.item())
+        if self.use_positional_encoding:
+            qkv = self.positional_encoding(qkv)
+        # use mask to hide future inputs
+        mask = self.get_attn_mask(len(attn_input))
+        # second output is attn weights
+        #attn_output, _ = self.attn(q, k, v, attn_mask=mask)
+        attn_output, _ = self.attn(qkv, qkv, qkv, attn_mask=mask)
+        if verbose:
+            print('F final word\'s attn output:')
+            for x in attn_output[-1, 0]:
+                print(x.item())
+        x = self.fc1(attn_output)
+        x = self.dropout(x)
+        x = self.relu(x)
+        x = self.fc2(x)
+        result = F.log_softmax(x, dim=2)
+        if verbose:
+            for i in range(result.shape[0]):
+                log_scores = result[i, 0]
+                scores = torch.exp(log_scores)
+                norm_scores = scores/sum(scores)
+                print('F ==== output for word {} ===='.format(i))
+                for x in norm_scores:
+                    print(x.item())
+        return result
+
