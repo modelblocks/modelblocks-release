9209210613c7df6e20c62f5d63b76f0185774c15
schuler.77 <schuler.77@unity-login1.asc.ohio-state.edu>
2021-08-26 15:41:47 -0400

    misc fixes to get cdr running on unity

--- resource-cdr/scripts/regress-cdr.sh
+++ resource-cdr/scripts/regress-cdr.sh
@@ -17,11 +17,11 @@ outfile="${base_filename/_part\./\.}"
 outdir+="_cdr.fitmodel_outdir";
 outfile+="_cdr.fitmodel";
 
-cat $3 | python3 ../resource-cdr/scripts/baseform_to_config.py $1 $2 $outdir $preds
-
 cdr_dir=$(cat ../config/user-cdr-directory.txt)
 
 export PYTHONPATH=$PYTHONPATH:$cdr_dir
+
+cat $3 | python3 ../resource-cdr/scripts/baseform_to_config.py $1 $2 $outdir $preds
 
 python3 -m cdr.bin.train $outdir/config.ini
 cp $outdir/CDR/summary.txt $outfile
--- resource-jlstm/jlstm_conda_env.sh
+++ resource-jlstm/jlstm_conda_env.sh
@@ -1,6 +1,6 @@
 # initialize and activate new conda environment, assumes working installation of miniconda3
 # compatible with TensorFlow 1.15 (https://github.com/tensorflow/models/tree/archive/research/lm_1b)
-conda create -n jlstm_env python=3.7 bazel -c conda-forge pandas
+conda create -n jlstm_env python=3.7 bazel pandas -c conda-forge
 
 # installs TensorFlow from pip
 CONDA_BASE=$(conda info --base)
