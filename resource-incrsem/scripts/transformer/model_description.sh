d5704cf00e5b88e3774c47b0f324348d7df7bbca
Christian Clark <clark.3664@osu.edu>
2021-09-24 11:44:06 -0400

    model description script save info about f model only (for faster grid search)

--- resource-incrsem/scripts/transformer/model_description.sh
+++ resource-incrsem/scripts/transformer/model_description.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
-# Prints the current git commit, F model ini, and J model ini for bookkeeping.
+## Prints the current git commit, F model ini, and J model ini for bookkeeping.
+#
+#F_INI=/home/clark.3664/git/modelblocks-release/resource-incrsem/scripts/besttrf.ini
+#J_INI=/home/clark.3664/git/modelblocks-release/resource-incrsem/scripts/besttrj.ini
+#
+#echo "==== F model INI ===="
+#cat $F_INI
+#
+#echo $'\n\n'"==== J model INI ===="
+#cat $J_INI
+#
+#echo $'\n\n'
+#git log -1
 
-F_INI=/home/clark.3664/git/modelblocks-release/resource-incrsem/scripts/besttrf.ini
-J_INI=/home/clark.3664/git/modelblocks-release/resource-incrsem/scripts/besttrj.ini
 
-echo "==== F model INI ===="
-cat $F_INI
+datetime=`date +%F_%T`
+mkdir $datetime
+git log -1 > $datetime/README
 
-echo $'\n\n'"==== J model INI ===="
-cat $J_INI
+fmodel=genmodel/wsj02to21.gcg15.long.morphed.-u10_-c0_-rp_besttr_ftrmodel
+fmodel_log=genmodel/wsj02to21.gcg15.long.morphed.-u10_-c0_-rp_besttr_ftrmodel.log
+ini=../resource-incrsem/scripts/besttrf.ini
 
-echo $'\n\n'
-git log -1
-
+cp $fmodel $fmodel_log $ini $datetime
