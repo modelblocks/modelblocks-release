41791bbd866e061a8170dc5e2084e294bea13d12
schuler.77 <schuler.77@deasy.ling.ohio-state.edu>
2021-11-25 16:26:27 -0500

    fix bug in semop calc for E when co-occurring with other ops

--- resource-rt/scripts/getSemOp.py
+++ resource-rt/scripts/getSemOp.py
@@ -1,4 +1,4 @@
-import os, sys
+import os, sys, re
 #sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..', 'resource-gcg', 'scripts'))
 #import tree
 
@@ -6,12 +6,14 @@ print( 'word opAa opAb opMa opMb opCa opCb opE opV opZ' )
 for line in sys.stdin:
     if( line.startswith('F') ):
         opAa,opAb,opMa,opMb,opCa,opCb,opE,opV,opZ = '0','0','0','0','0','0','0','0','0'
-        if( line.split()[-1].split('&')[1] != '' and line.split()[-1].split('&')[1][0] in 'M0123456789' ): opE='1'
+#        if( line.split()[-1].split('&')[1] != '' and line.split()[-1].split('&')[1][0] in 'M0123456789' ): opE='1'
+        if( 'M' in line.split()[-1].split('&')[1] or re.search( '\d', line.split()[-1].split('&')[1] ) ): opE='1'
         if( 'V' in line.split()[-1].split('&')[1] ): opV='1'
         if( 'Z' in line.split()[-1].split('&')[1] ): opZ='1'
     if( line.startswith('W') ): w = line.split()[-1]
     if( line.startswith('J') ):
-        if( line.split()[-1].split('&')[1] != '' and line.split()[-1].split('&')[1][0] in 'M0123456789' ): opE='1'
+#        if( line.split()[-1].split('&')[1] != '' and line.split()[-1].split('&')[1][0] in 'M0123456789' ): opE='1'
+        if( 'M' in line.split()[-1].split('&')[1] or re.search( '\d', line.split()[-1].split('&')[1] ) ): opE='1'
         if( 'V' in line.split()[-1].split('&')[1] ): opV='1'
         if( 'Z' in line.split()[-1].split('&')[1] ): opZ='1'
         if( line.split()[-1].split('&')[2][0] in '0123456789' ): opAa='1'
