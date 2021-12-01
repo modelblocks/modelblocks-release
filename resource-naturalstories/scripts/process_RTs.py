d7b8e707a35d5ddac64f2538b41b776bdc50bf5b
schuler.77 <schuler.77@deasy.ling.ohio-state.edu>
2021-12-01 18:45:22 -0500

    move natstories fix to mb from ns repo

---
+++ resource-naturalstories/scripts/process_RTs.py
@@ -0,0 +1,24 @@
+import sys
+import pandas
+
+offset = 230
+
+X1 = pandas.read_csv( sys.argv[1], index_col=False )
+X2 = pandas.read_csv( sys.argv[2], index_col=False )
+W  = pandas.read_csv( sys.argv[3], sep='\t', index_col=False )
+X = X1.append(X2)
+
+## Remove the 231st item, which was an empty token in the orig expt that resulted in the misalignment
+X = X[ ~((X['item']==3) & (X['zone']==offset+1)) ]
+## Shift the "zone" (index) down by two for all words
+X[ 'zone' ] -= 2
+## Shift the "zone" (index) down by an additional one for all words in story 3 following the empty token
+#X.loc[ (X['item']==3) & (X['zone']>offset), 'zone' ] -= 1
+
+X = X.merge(W,how='inner')  #,on=['item','zone'])
+
+X = X[ (X['RT']>100) & (X['RT']<3000) & (X['correct']>4) ]
+
+X.to_csv( sys.stdout, sep ='\t', index=False )
+
+
