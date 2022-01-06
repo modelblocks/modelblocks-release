ba804b59b82bd014c5506b46b8761eff189181b9
Christian Clark <clark.3664@osu.edu>
2022-01-06 15:11:53 -0500

    new semop variable, opEnoV, for non-passive E operations

--- resource-rt/scripts/getSemOp.py
+++ resource-rt/scripts/getSemOp.py
@@ -2,10 +2,10 @@ import os, sys, re
 #sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..', 'resource-gcg', 'scripts'))
 #import tree
 
-print( 'word opAa opAb opMa opMb opCa opCb opE opV opZ' )
+print( 'word opAa opAb opMa opMb opCa opCb opE opEnoV opV opZ' )
 for line in sys.stdin:
     if( line.startswith('F') ):
-        opAa,opAb,opMa,opMb,opCa,opCb,opE,opV,opZ = '0','0','0','0','0','0','0','0','0'
+        opAa,opAb,opMa,opMb,opCa,opCb,opE,opEnoV,opV,opZ = '0','0','0','0','0','0','0','0','0','0'
 #        if( line.split()[-1].split('&')[1] != '' and line.split()[-1].split('&')[1][0] in 'M0123456789' ): opE='1'
         if( 'M' in line.split()[-1].split('&')[1] or re.search( '\d', line.split()[-1].split('&')[1] ) ): opE='1'
         if( 'V' in line.split()[-1].split('&')[1] ): opV='1'
@@ -22,7 +22,8 @@ for line in sys.stdin:
         if( line.split()[-1].split('&')[3] == 'M' ): opMb='1'
         if( line.split()[-1].split('&')[2] == 'C' ): opCa='1'
         if( line.split()[-1].split('&')[3] == 'C' ): opCb='1'
-        print( w + ' ' + opAa + ' ' + opAb + ' ' + opMa + ' ' + opMb + ' ' + opCa + ' ' + opCb + ' ' + opE + ' ' + opV + ' ' + opZ )
+        if opE == '1' and opV == '0': opEnoV='1'
+        print( w + ' ' + opAa + ' ' + opAb + ' ' + opMa + ' ' + opMb + ' ' + opCa + ' ' + opCb + ' ' + opE + ' ' + opEnoV + ' ' + opV + ' ' + opZ )
 
 #    if (line.strip() !='') and (line.strip()[0] != '%'):
 #        T = tree.Tree()
