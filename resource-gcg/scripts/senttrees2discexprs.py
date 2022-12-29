334fbaedbee27398645aae9f6d65e8e9c71f6317
William Schuler <schuler.77@osu.edu>
2022-12-28 21:48:41 -0500

    logic translation for trees

--- resource-gcg/scripts/senttrees2discexprs.py
+++ resource-gcg/scripts/senttrees2discexprs.py
@@ -21,26 +21,50 @@ def getLemma( c, w ):
   return eqns
 
 
-ScopesDown = {}
-def getScopes( t ):
-  ScopesDown = {}
-
-
 nSent = 0
 nWord = 0
-def translate( t, lVars ):
-  global nSent
+
+
+ScopesDown = {}
+def getScopes( t ):
   global nWord
 
-  if len(t.ch) == 1 and len(t.ch[0].ch) == 0:
-    nWord += 1
-    pred = getLemma( t.c, t.ch[0].c )
-    print( nSent, nWord, pred )
-#  else:
-#    if '-lA' in t.ch[0]:
+  ScopesDown = {}
 
   for st in t.ch:
-    translate( st, lVars )
+    getScopes( st )
+
+  if   len(t.ch) == 0: t.sVar = 'x' + str(nWord)
+  elif len(t.ch) == 1: t.sVar = t.ch[0].sVar
+  elif len(t.ch) == 2: t.sVar = t.ch[0].sVar if '-l' not in t.ch[0].c else t.ch[1].sVar if '-l' not in t.ch[1].c else None
+  else: print( 'ERROR: too many children in ', t )
+
+
+def translate( t, lsVars=[] ):
+#  global nSent
+#  global nWord
+
+  if len(t.ch) == 1 and len(t.ch[0].ch) == 0:
+#    nWord += 1
+    pred = getLemma( t.c, t.ch[0].c )
+#    print( nSent, nWord, pred )
+    return( 'True' if pred == '' else pred + ' ' + t.sVar + ' '.join(lsVars) )
+  elif len(t.ch) == 1:
+    return( translate(t.ch[0],lsVars) )
+  elif len(t.ch) == 2:
+    if   '-lA' in t.ch[0].c:  return( '[' + translate(t.ch[0]) + '] (\\' + t.ch[0].sVar + ' ' + translate(t.ch[1],[t.ch[0].sVar]+lsVars) + ')' )
+    elif '-lA' in t.ch[1].c:  return( '[' + translate(t.ch[1]) + '] (\\' + t.ch[1].sVar + ' ' + translate(t.ch[0],[t.ch[1].sVar]+lsVars) + ')' )
+    elif '-lU' in t.ch[0].c:  return( '[' + translate(t.ch[0]) + '] (\\' + t.ch[0].sVar + ' ' + translate(t.ch[1],[t.ch[0].sVar]+lsVars) + ')' )
+    elif '-lU' in t.ch[1].c:  return( '[' + translate(t.ch[1]) + '] (\\' + t.ch[1].sVar + ' ' + translate(t.ch[0],[t.ch[1].sVar]+lsVars) + ')' )
+    elif '-lM' in t.ch[0].c:  return( translate(t.ch[0],[t.ch[1].sVar]) + ' ^ ' + translate(t.ch[1],lsVars) )
+    elif '-lM' in t.ch[1].c:  return( translate(t.ch[0],lsVars) + ' ^ ' + translate(t.ch[1],[t.ch[0].sVar]) )
+    elif '-lC' in t.ch[0].c:  return( translate(t.ch[0],lsVars) + ' ^ ' + translate(t.ch[1],lsVars) )
+    elif '-lC' in t.ch[1].c:  return( translate(t.ch[0],lsVars) + ' ^ ' + translate(t.ch[1],lsVars) )
+    else: print( 'ERROR: unhandled rule in ', t )
+  else: print( 'ERROR: too many children in ', t )
+#
+#  for st in t.ch:
+#    translate( st, lVars )
 
 for line in sys.stdin:
 
@@ -56,6 +80,6 @@ for line in sys.stdin:
     nWord = 0
 
     getScopes( t )
-    print( translate(t,nSent) )
+    print( translate(t) )
 
 
