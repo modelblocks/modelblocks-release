32baa4d580896f11edaee9e287a89625d406df45
William Schuler <schuler.77@osu.edu>
2023-01-08 23:38:05 -0500

    fix indent

--- resource-gcg/scripts/indent.py
+++ resource-gcg/scripts/indent.py
@@ -4,30 +4,31 @@ OPENS = '(['
 CLOSES = ')]'
 
 Indents = [0]
-ready = None
+followstate = None
+## For each line...
 for nLine,line in enumerate( sys.stdin ):
   i = 0
+  ## For each character...
   for c in line:
+    ## If open delimiter, record tab...
     if c in OPENS:
       sys.stdout.write( c )
       i += 1
+      if followstate == '(':  Indents[-1] = i-1
       Indents.append(i)
-      ready = '('
+      followstate = '('
+    ## If close delimiter, set follow state...
     elif c in CLOSES:
       if len(Indents) == 0:  sys.stdout.write( 'ERROR: Too many \'' + c + '\' in line ' + str(nLine) + ': ' + line + '\n' )
       sys.stdout.write( c )
       i += 1
       Indents.pop()
-      ready = ')'
-    elif c == ' ' and ready == '(':
-      sys.stdout.write( c )
-      i += 1
-      Indents[-1] = i
-      ready = None
-    elif c in ' \t\n' and ready == ')':
+      followstate = ')'
+    ## If whitespace following close delimiter...
+    elif c in ' \t\n' and followstate == ')':
       sys.stdout.write( '\n' + ' '*Indents[-1] )
       i = Indents[-1]
-      ready = None
+      followstate = None
     else:
       sys.stdout.write( c )
       i += 1
