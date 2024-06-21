45e89a25822c5dd7babd1b66301b4f529a9d26c6
William Schuler <schuler.77@osu.edu>
2024-06-21 15:58:42 -0400

    fixes for kenlm and linetrees

--- resource-kenlm/scripts/unigram.py
+++ resource-kenlm/scripts/unigram.py
@@ -9,13 +9,15 @@ with open(sys.argv[1],'r') as f:
     if len(line) > 0 and line[0] == '-':
       ##only load lines that begin with logprobs
       sline = line.strip().split()
-      model[sline[1]] = float(sline[0])
+      if len(sline) > 1: model[sline[1]] = float(sline[0])
 
 with open(sys.argv[2],'r') as f:
   for line in f:
     for word in line.strip().split():
       if word in model:
-        print '%s %s' %(word, -model[word])
+        print( word + ' ' + str(-model[word]) )
+        #print '%s %s' %(word, -model[word])
       else:
         ##if we don't know a word, treat as UNK
-        print '%s %s' %(word, -model['<unk>'])
+        print( word + ' ' + str(-model['<unk>']) )
+        #print '%s %s' %(word, -model['<unk>'])
