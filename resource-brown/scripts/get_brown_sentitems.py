fd2880328b073b46a7466114b5819aef996c9414
Byung-Doh Oh <oh.531@osu.edu>
2024-10-31 13:08:09 -0400

    add ARTICLE to brown SPR

--- resource-brown/scripts/get_brown_sentitems.py
+++ resource-brown/scripts/get_brown_sentitems.py
@@ -2,10 +2,9 @@ import sys
 import pandas as pd
 
 df = pd.read_csv(sys.stdin, sep=' ', keep_default_na=False)
-gb = df.groupby('sentid')
-curr_text_id = -1
-for _, _df in gb:
-    if list(_df.text_id)[0] != curr_text_id:
-        print("!ARTICLE")
-        curr_text_id = list(_df.text_id)[0]
-    print(' '.join(list(_df.word)))
+gb = df.groupby("docid")
+for _, df_doc in gb:
+    print("!ARTICLE")
+    gb_sent = df_doc.groupby("sentid")
+    for _, df_sent in gb_sent:
+        print(' '.join(list(df_sent.word)))
