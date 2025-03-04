04d0cac9009ee4d741e3539f14d08dbf8f59dd17
byungdoh <oh.b@nyu.edu>
2025-03-03 21:11:11 -0500

    wordfreq update

--- resource-wordfreq/scripts/unigram.py
+++ resource-wordfreq/scripts/unigram.py
@@ -4,9 +4,20 @@ from wordfreq import word_frequency
 
 print("word unigramsurp")
 
+lang_dict = {"du": "nl",
+             "ge": "de",
+             "gr": "el",
+             "no": "nb",
+             "sp": "es"}
+
 lang = sys.argv[1]
+if lang in lang_dict:
+    lang = lang_dict[lang]
+
 with open(sys.argv[2], "r") as f:
     for line in f:
+        if line.strip() == "!ARTICLE":
+            continue
         for word in line.strip().split():
             # uni_surp = -1 * math.log2(word_frequency(word, lang, wordlist='best', minimum=0.0))
             uni_surp = -1 * math.log2(word_frequency(word, lang, wordlist='best', minimum=1e-10))
